c776a24b3667704873a6c634f89b1dbe624cf1bc
KalkM <106752167+KalkM@users.noreply.github.com>
2022-09-07 21:52:28 +0300

    Str

--- str_func.c
+++ str_func.c
@@ -81,7 +81,6 @@ char *_strncat(char *dest, const char *src, size_t n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
-
 	return (dest);
 }
 
